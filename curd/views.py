ba64478b42f3efa129fe1e4371000329bacb4a0b
Pavel khan <78027385+Pavel-Khan17@users.noreply.github.com>
2024-01-23 17:22:25 +0600

    read operation added

--- curd/views.py
+++ curd/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import studentModel
 # Create your views here.
 
 
+#  Read operation 
+"""showing all the student list """
 def home(request):
-  
-  context={}
+  students = studentModel.objects.all()
+  context={'students' : students}
   return render(request, "home.html", context)
