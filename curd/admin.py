5b302c61fa850c5de49e7a34e39689a3931a857c
Pavel khan <78027385+Pavel-Khan17@users.noreply.github.com>
2024-01-23 12:29:37 +0600

    added models

--- curd/admin.py
+++ curd/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import studentModel, departmentModel
 # Register your models here.
+
+@admin.register(studentModel)
+class studentModelAdmin(admin.ModelAdmin):
+  list_display = ['id', 'name', 'email']
+
+
+admin.site.register(departmentModel)
--- curd/models.py
+++ curd/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 
@@ -13,7 +13,8 @@ class studentModel(models.Model):
   name            = models.CharField(max_length=50)
   email           = models.EmailField(max_length=254, unique=True)
   department_name = models.ForeignKey(departmentModel, on_delete=models.CASCADE, related_name="department")
-  profile = models.ImageField(upload_to='profile', height_field=None, width_field=None, max_length=None, blank=True)
-  
+  profile         = models.ImageField(upload_to='profile', height_field=None, width_field=None, max_length=None, blank=True)
+  created_date    = models.DateField(auto_now_add=True)
+  updated_date    = models.DateField(auto_now=True)
   def __str__(self) -> str:
     return self.name
