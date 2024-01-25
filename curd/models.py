58a7646edaad695a59ad0800c94006349c1a3263
Pavel khan <78027385+Pavel-Khan17@users.noreply.github.com>
2024-01-25 19:14:53 +0600

    male female tab search added

--- curd/models.py
+++ curd/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from .constant import GENDER_CHOICE
 # Create your models here.
 
 
@@ -14,6 +15,7 @@ class studentModel(models.Model):
   email           = models.EmailField(max_length=254, unique=True)
   department_name = models.ForeignKey(departmentModel, on_delete=models.CASCADE, related_name="department")
   profile         = models.ImageField(upload_to='profile', height_field=None, width_field=None, max_length=None, blank=True)
+  gender          = models.CharField(max_length=1, choices=GENDER_CHOICE, null=True, blank=True)
   created_date    = models.DateField(auto_now_add=True)
   updated_date    = models.DateField(auto_now=True)
   def __str__(self) -> str:
--- curd/views.py
+++ curd/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from .models import studentModel
 from .forms import studentModelForm
 # Create your views here.
@@ -7,7 +8,22 @@ from .forms import studentModelForm
 #  Read operation 
 """showing all the student list """
 def home(request):
-  students = studentModel.objects.all()
+  if request.method == 'GET':
+    q = request.GET.get('q')
+    if q == 'M':
+      students = studentModel.objects.filter(gender='M')
+    elif q == 'F':
+      students = studentModel.objects.filter(gender='F')
+    elif q is not None:
+      students = studentModel.objects.filter(
+        Q(name__icontains=q)|
+        Q(email__icontains=q)|
+        Q(department_name__name__icontains=q)|
+        Q(gender__exact=q)
+      )
+      print(students)
+    else:
+      students = studentModel.objects.all().order_by('-updated_date')
   context={'students' : students}
   return render(request, "home.html", context)
 
